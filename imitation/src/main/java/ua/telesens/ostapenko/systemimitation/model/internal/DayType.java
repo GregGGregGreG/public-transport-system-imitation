17a7af1e64370ad6ff6b9f471cd91b887f0c8fde
root <truegreg13@mail.ru>
2016-01-04 18:15:54 +0200

    add dayType to

--- imitation/src/main/java/ua/telesens/ostapenko/systemimitation/model/internal/DayType.java
+++ imitation/src/main/java/ua/telesens/ostapenko/systemimitation/model/internal/DayType.java
@@ -1,11 +1,19 @@
 package ua.telesens.ostapenko.systemimitation.model.internal;
 
+import java.time.DayOfWeek;
+
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
+
 /**
  * @author root
  * @since 29.12.15
  */
 public enum DayType {
+    //// FIXME: 04.01.16 Write logic from customise day
+    WORKDAY, HOLIDAY, CUSTOMISE;
 
-    WORKDAY, HOLIDAY, CUSTOMISE
-
+    public static DayType to(DayOfWeek dayOfWeek) {
+        return dayOfWeek.equals(SATURDAY) || dayOfWeek.equals(SUNDAY) ? HOLIDAY : WORKDAY;
+    }
 }
---
+++ imitation/src/test/java/ua/telesens/ostapenko/systemimitation/model/internal/DayTypeTest.java
@@ -0,0 +1,21 @@
+package ua.telesens.ostapenko.systemimitation.model.internal;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+/**
+ * @author root
+ * @since 04.01.16
+ */
+public class DayTypeTest {
+
+    @Test
+    public void to() throws Exception {
+        LocalDateTime time = LocalDateTime.of(2015, Month.NOVEMBER, 12, 6, 0);
+        TestCase.assertEquals(DayType.WORKDAY, DayType.to(time.getDayOfWeek()));
+
+    }
+}
