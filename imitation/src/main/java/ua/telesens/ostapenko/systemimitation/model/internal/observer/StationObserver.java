e6daa623f649feee7529c34293e61b6ef9d9290a
root <truegreg13@mail.ru>
2016-01-19 19:10:39 +0200

    Remove parallel from add passenger to station

--- imitation/src/main/java/ua/telesens/ostapenko/systemimitation/model/internal/observer/StationObserver.java
+++ imitation/src/main/java/ua/telesens/ostapenko/systemimitation/model/internal/observer/StationObserver.java
@@ -47,7 +47,7 @@ public class StationObserver implements SystemImitationObserver, PassengerObserv
     }
 
     public void addPassenger(List<Passenger> passengers) throws IllegalStateException {
-        passengers.stream().parallel().forEach(this::addPassenger);
+        passengers.stream().forEach(this::addPassenger);
     }
 
     public void addPassenger(Passenger passenger) throws IllegalStateException {
