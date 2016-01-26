4e803b1483c2599de21e05dab4924463bfe4005a
root <truegreg13@mail.ru>
2016-01-26 11:03:35 +0200

    Include creater jar app in module

---
+++ console-app/src/main/java/ua/telesens/ostapenko/consoleapp/Bootstrap.java
@@ -0,0 +1,114 @@
+package ua.telesens.ostapenko.consoleapp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/**
+ * @author root
+ * @since 23.01.16
+ */
+public final class Bootstrap {
+
+    private static final String APPLICATION_PROPERTIES = "application.properties";
+    private String PROPERTY_NAME_ROOT_FOLDER;
+    private String PROPERTY_NAME_LOG_FOLDER;
+    private String PROPERTY_NAME_LOG_FILE;
+    private String PROPERTY_NAME_REPORT_FOLDER;
+
+    public void installPatch() {
+        readProperties();
+        String workingDir = System.getProperty("user.dir");
+        showMessage("Current operation system ", getOsName(), false);
+        showMessage("Current working directory ", workingDir, false);
+
+        Path path = Paths.get(workingDir);
+        Path fileName = path.getFileName();
+        System.out.println(fileName);
+        Path parent = path.getParent();
+        System.out.println(parent);
+
+        if (fileName.toString().equals(PROPERTY_NAME_ROOT_FOLDER)) {
+            // logger folder
+            Path log = Paths.get(parent + "/" + PROPERTY_NAME_LOG_FOLDER);
+            if (!Files.exists(log)) {
+                try {
+                    Files.createDirectories(log);
+                    showMessage("Directory from log created!", log.toUri().getPath(), false);
+                } catch (IOException var1) {
+                    showMessage("Error log folder created!", var1.getMessage(), true);
+                    var1.printStackTrace();
+                    System.exit(2);
+                }
+            } else {
+                showMessage("Directory log is exists!", log.toUri().getPath(), false);
+            }
+            System.setProperty("logfile.path", log.toAbsolutePath() + "/" + PROPERTY_NAME_LOG_FILE);
+
+            // report folder
+            Path report = Paths.get(parent + "/" + PROPERTY_NAME_REPORT_FOLDER);
+            if (!Files.exists(report)) {
+                try {
+                    Files.createDirectories(report);
+                    showMessage("Directory from report created!", report.toUri().getPath(), false);
+                } catch (IOException var2) {
+                    showMessage("Error report folder created!", var2.getMessage(), true);
+                    var2.printStackTrace();
+                    System.exit(2);
+                }
+            } else {
+                showMessage("Directory report is exists!", report.toUri().getPath(), false);
+            }
+            System.setProperty("path.report.folder", report.toAbsolutePath().toString());
+        } else {
+            showMessage("Not found root folder", PROPERTY_NAME_ROOT_FOLDER, false);
+            System.exit(3);
+        }
+    }
+
+    private String getOsName() {
+        return System.getProperty("os.name") +
+                "\t, version: " + System.getProperty("os.version") +
+                "\t, arch: " + System.getProperty("os.arch");
+    }
+
+    private void readProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = getClass().getClassLoader().getResourceAsStream(APPLICATION_PROPERTIES);
+            // load a properties file
+            prop.load(input);
+            // get the property value
+            PROPERTY_NAME_ROOT_FOLDER = prop.getProperty("name.root.folder");
+            PROPERTY_NAME_LOG_FOLDER = prop.getProperty("name.log.folder");
+            PROPERTY_NAME_REPORT_FOLDER = prop.getProperty("name.report.folder");
+            PROPERTY_NAME_LOG_FILE = prop.getProperty("name.log.file");
+
+        } catch (IOException var3) {
+            showMessage("Error read properties", var3.getMessage(), true);
+            var3.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException var4) {
+                    showMessage("Error read properties", var4.getMessage(), true);
+                    var4.printStackTrace();
+                    System.exit(3);
+                }
+            }
+        }
+    }
+
+    private static void showMessage(String title, String message, boolean error) {
+        PrintStream stream = error ? System.err : System.out;
+        stream.println("\n" + title + ": " + message);
+    }
+}
+
